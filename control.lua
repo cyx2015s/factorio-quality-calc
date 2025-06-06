224c44592814f342da711397afcfb3fd5badb920
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2025-06-06 18:58:55 +0800

    revert the prototype name, dedent some code

--- control.lua
+++ control.lua
@@ -78,90 +78,92 @@ function update_matrix_result_gui(player_index, recalc)
         end
     end
 end
-script.on_event(defines.events.on_lua_shortcut, function(event)
-    if event.prototype_name == "qct.main" then
-        local player = game.get_player(event.player_index)
-        if not player then
-            return
-        end
-        local gui_root = player.gui.screen
-        if gui_root["qct.main"] then
-            gui_root["qct.main"].destroy()
-        else
-            local frame = gui_root.add {
-                type = "frame",
-                name = "qct.main",
-                caption = { "qct.caption" },
-                direction = "vertical",
-                auto_center = true,
-            }
-            frame.style.minimal_width = 400
-            frame.style.minimal_height = 300
-            frame.style.maximal_width = 800
-            frame.style.maximal_height = 600
-            frame = frame.add {
-                type = "flow",
-                name = "qct.flow",
-                direction = "vertical",
-            }
-            frame.style.vertical_spacing = 12
-            local input = frame.add {
-                type = "frame",
-                name = "qct.input",
-                direction = "vertical",
-                style = "inside_shallow_frame_with_padding"
-            }
-            input.style.vertically_stretchable = true
-            input.style.horizontally_stretchable = true
-            gui.create_text_input_with_label(input, "qct.craft-quality-bonus")
-            gui.create_text_input_with_label(input, "qct.recycle-quality-bonus")
-            gui.create_text_input_with_label(input, "qct.craft-production-multiplier")
-            gui.create_text_input_with_label(input, "qct.recycle-production-multiplier")
 
-            gui.create_checkbox_with_label(input, "qct.bruteforce-recycle")
-            local result = frame.add {
-                type = "frame",
-                name = "qct.result",
-                direction = "vertical",
-                style = "inside_shallow_frame_with_padding",
-            }
-            result.style.horizontal_align = "center"
+script.on_event(defines.events.on_lua_shortcut, function(event)
+    if event.prototype_name ~= "qct-main" then
+        return
+    end
+    local player = game.get_player(event.player_index)
+    if not player then
+        return
+    end
+    local gui_root = player.gui.screen
+    if gui_root["qct.main"] then
+        gui_root["qct.main"].destroy()
+    else
+        local frame = gui_root.add {
+            type = "frame",
+            name = "qct.main",
+            caption = { "qct.caption" },
+            direction = "vertical",
+            auto_center = true,
+        }
+        frame.style.minimal_width = 400
+        frame.style.minimal_height = 300
+        frame.style.maximal_width = 800
+        frame.style.maximal_height = 600
+        frame = frame.add {
+            type = "flow",
+            name = "qct.flow",
+            direction = "vertical",
+        }
+        frame.style.vertical_spacing = 12
+        local input = frame.add {
+            type = "frame",
+            name = "qct.input",
+            direction = "vertical",
+            style = "inside_shallow_frame_with_padding"
+        }
+        input.style.vertically_stretchable = true
+        input.style.horizontally_stretchable = true
+        gui.create_text_input_with_label(input, "qct.craft-quality-bonus")
+        gui.create_text_input_with_label(input, "qct.recycle-quality-bonus")
+        gui.create_text_input_with_label(input, "qct.craft-production-multiplier")
+        gui.create_text_input_with_label(input, "qct.recycle-production-multiplier")
+
+        gui.create_checkbox_with_label(input, "qct.bruteforce-recycle")
+        local result = frame.add {
+            type = "frame",
+            name = "qct.result",
+            direction = "vertical",
+            style = "inside_shallow_frame_with_padding",
+        }
+        result.style.horizontal_align = "center"
+        result.add {
+            type = "label",
+            caption = { "qct.result-caption" },
+        }
+        local result = result.add {
+            type = "table",
+            name = "qct.result",
+            column_count = 2,
+            draw_horizontal_line_after_headers = true,
+            draw_vertical_lines = true,
+            vertical_centering = true
+        }
+        result.style.vertically_stretchable = true
+        result.style.horizontally_stretchable = true
+        result.add {
+            type = "label",
+            caption = { "qct.quality-column" }
+        }
+        result.add {
+            type = "label",
+            caption = { "qct.probability-column", "[quality=" .. cache.quality_prototype_name[#cache.quality_prototype_name] .. "]" },
+        }
+        for i, name in pairs(cache.quality_name) do
+            local prototype_name = cache.quality_prototype_name[i]
             result.add {
                 type = "label",
-                caption = { "qct.result-caption" },
-            }
-            local result = result.add {
-                type = "table",
-                name = "qct.result",
-                column_count = 2,
-                draw_horizontal_line_after_headers = true,
-                draw_vertical_lines = true,
-                vertical_centering = true
-            }
-            result.style.vertically_stretchable = true
-            result.style.horizontally_stretchable = true
-            result.add {
-                type = "label",
-                caption = { "qct.quality-column" }
+                caption = { "", "[quality=" .. prototype_name .. "]", name },
             }
             result.add {
                 type = "label",
-                caption = { "qct.probability-column", "[quality=" .. cache.quality_prototype_name[#cache.quality_prototype_name] .. "]" },
+                name = "qct.result-" .. i,
+                caption = "0.0",
             }
-            for i, name in pairs(cache.quality_name) do
-                local prototype_name = cache.quality_prototype_name[i]
-                result.add {
-                    type = "label",
-                    caption = { "", "[quality=" .. prototype_name .. "]", name },
-                }
-                result.add {
-                    type = "label",
-                    name = "qct.result-" .. i,
-                    caption = "0.0",
-                }
-            end
-            update_matrix_result_gui(event.player_index, false)
         end
+        update_matrix_result_gui(event.player_index, false)
     end
 end)
 
