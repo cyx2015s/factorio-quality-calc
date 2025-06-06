814bf7dbb1f5eca4f0da880c1fbb9f98b9e22320
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2025-06-06 18:57:40 +0800

    more consistent internal name

--- control.lua
+++ control.lua
@@ -64,12 +64,12 @@ function update_matrix_result_gui(player_index, recalc)
         storage_util.set(player_index, "result-machine-count", machine_count)
     end
     ::main::
-    if player.gui.screen["qct-main"] then
+    if player.gui.screen["qct.main"] then
         ---@class LuaGuiElement
-        local result_flow = player.gui.screen["qct-main"]["qct-flow"]["qct-result"]
-        local result_table = result_flow["qct-result"]
+        local result_flow = player.gui.screen["qct.main"]["qct.flow"]["qct.result"]
+        local result_table = result_flow["qct.result"]
         for i = 1, #cache.quality_next_probability do
-            local matrix_element = matrix.matrix_get()
+            local matrix_element = err and 0.0 or matrix.matrix_get()
             result_table.children[i * 2 + 2].caption = string.format(
                 "%.2f%% (%.2f : 1)",
                 matrix.matrix_get(storage_util.get(player_index, "result-converged-matrix"), i, 5) * 100,
@@ -79,18 +79,18 @@ function update_matrix_result_gui(player_index, recalc)
     end
 end
 script.on_event(defines.events.on_lua_shortcut, function(event)
-    if event.prototype_name == "qct-main" then
+    if event.prototype_name == "qct.main" then
         local player = game.get_player(event.player_index)
         if not player then
             return
         end
         local gui_root = player.gui.screen
-        if gui_root["qct-main"] then
-            gui_root["qct-main"].destroy()
+        if gui_root["qct.main"] then
+            gui_root["qct.main"].destroy()
         else
             local frame = gui_root.add {
                 type = "frame",
-                name = "qct-main",
+                name = "qct.main",
                 caption = { "qct.caption" },
                 direction = "vertical",
                 auto_center = true,
@@ -101,13 +101,13 @@ script.on_event(defines.events.on_lua_shortcut, function(event)
             frame.style.maximal_height = 600
             frame = frame.add {
                 type = "flow",
-                name = "qct-flow",
+                name = "qct.flow",
                 direction = "vertical",
             }
             frame.style.vertical_spacing = 12
             local input = frame.add {
                 type = "frame",
-                name = "qct-input",
+                name = "qct.input",
                 direction = "vertical",
                 style = "inside_shallow_frame_with_padding"
             }
@@ -121,7 +121,7 @@ script.on_event(defines.events.on_lua_shortcut, function(event)
             gui.create_checkbox_with_label(input, "qct.bruteforce-recycle")
             local result = frame.add {
                 type = "frame",
-                name = "qct-result",
+                name = "qct.result",
                 direction = "vertical",
                 style = "inside_shallow_frame_with_padding",
             }
@@ -132,7 +132,7 @@ script.on_event(defines.events.on_lua_shortcut, function(event)
             }
             local result = result.add {
                 type = "table",
-                name = "qct-result",
+                name = "qct.result",
                 column_count = 2,
                 draw_horizontal_line_after_headers = true,
                 draw_vertical_lines = true,
@@ -156,7 +156,7 @@ script.on_event(defines.events.on_lua_shortcut, function(event)
                 }
                 result.add {
                     type = "label",
-                    name = "qct-result-" .. i,
+                    name = "qct.result-" .. i,
                     caption = "0.0",
                 }
             end
